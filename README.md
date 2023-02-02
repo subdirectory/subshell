6d282ae7774f6aec524e4156c1fa6ce254a83288
subshell <subshell@subshell.xyz>
2023-02-02 11:46:36 +0000

    deno.land/x/subshell@0.2.25

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ in a Deno 🦕 repl, using polkadot-js extension as remote signer ✍️.
 Start with browser extension integration
 
 ```
-$　deno repl --unstable --eval-file=https://deno.land/x/subshell@0.2.24/init.ts
+$　deno repl --unstable --eval-file=https://deno.land/x/subshell@0.2.25/init.ts
 ```
 
 Start in lite mode
 
 ```
-$　deno repl --eval-file=https://deno.land/x/subshell@0.2.24/tini.ts
+$　deno repl --eval-file=https://deno.land/x/subshell@0.2.25/tini.ts
 ```
--- init.ts
+++ init.ts
@@ -1,10 +1,10 @@
-import "https://deno.land/x/polkadot@0.2.24/api-augment/mod.ts";
+import "https://deno.land/x/polkadot@0.2.25/api-augment/mod.ts";
 import {
   ApiPromise,
   WsProvider,
-} from "https://deno.land/x/polkadot@0.2.24/api/mod.ts";
-import { Client } from "https://deno.land/x/subshell@0.2.24/client/mod.ts";
-// import { VerboseSigner } from "https://deno.land/x/subshell@0.2.24/signer/mod.ts";
+} from "https://deno.land/x/polkadot@0.2.25/api/mod.ts";
+import { Client } from "https://deno.land/x/subshell@0.2.25/client/mod.ts";
+// import { VerboseSigner } from "https://deno.land/x/subshell@0.2.25/signer/mod.ts";
 
 const SESSION_ID = Deno.env.get("SESSION_ID") ?? "";
 const PROVIDER = Deno.env.get("PROVIDER") ?? "wss://rpc.polkadot.io";
