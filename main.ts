9a940ed50df2c04e081830385454fc8f61413edf
subshell <subshell@subshell.xyz>
2023-03-25 12:33:09 +0000

    deno.land/x/subshell@0.2.33

--- main.ts
+++ main.ts
@@ -8,6 +8,6 @@ const status = await Deno.run({
     "--allow-env",
     "--no-prompt",
     "--unstable",
-    "--eval-file=https://deno.land/x/subshell@0.2.32/init.ts",
+    "--eval-file=https://deno.land/x/subshell@0.2.33/init.ts",
   ],
 }).status();
