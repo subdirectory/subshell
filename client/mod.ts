6f92c214b7273f7b6da3d4b60e3813bb62806c34
subshell <subshell@subshell.xyz>
2023-05-16 06:56:27 +0000

    deno.land/x/subshell@0.2.39

--- client/mod.ts
+++ client/mod.ts
@@ -1,16 +1,16 @@
 import {
   ApiPromise,
   WsProvider,
-} from "https://deno.land/x/polkadot@0.2.38/api/mod.ts";
+} from "https://deno.land/x/polkadot@0.2.39/api/mod.ts";
 import type {
   Signer,
   SignerResult,
-} from "https://deno.land/x/polkadot@0.2.38/api/types/index.ts";
+} from "https://deno.land/x/polkadot@0.2.39/api/types/index.ts";
 import type {
   Registry,
   SignerPayloadJSON,
   SignerPayloadRaw,
-} from "https://deno.land/x/polkadot@0.2.38/types/types/index.ts";
+} from "https://deno.land/x/polkadot@0.2.39/types/types/index.ts";
 
 export class Client implements Signer {
   private encoder = new TextEncoder();
